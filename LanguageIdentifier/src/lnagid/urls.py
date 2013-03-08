7b3938806a0ef83ad0d0857ad38a8b2ab02bcc4f
netzsooc <netzsooc@gmail.com>
2013-03-08 16:57:35 -0600

    Update urls.py
    modifica user agent

--- LanguageIdentifier/src/lnagid/urls.py
+++ LanguageIdentifier/src/lnagid/urls.py
@@ -8,5 +8,5 @@ import urllib2
 
 def open_as_mozilla(url):
     arch = urllib2.Request(url)
-    arch.add_header('User-agent', 'Mozilla/5.0')
+    arch.add_header('User-agent', 'Mozilla/5.0 (X11; Archlinux; Linux i686; rv:15.0) Gecko/20100101 Firefox/18.0.0')
     return urllib2.urlopen(url)
